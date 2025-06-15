3ce99ff8ce1929b6d3ed11643964950feaddabf5
Marcel Wienöbst <marcel.wienoebst@gmx.de>
2025-06-15 21:49:53 +0200

    fix remaining note

--- ciflyr/src/rust/src/lib.rs
+++ ciflyr/src/rust/src/lib.rs
@@ -46,7 +46,7 @@ fn reach(
     ruletable: Robj,
     #[default = "FALSE"] tableAsString: bool,
     #[default = "FALSE"] verbose: bool,
-) -> Vec<usize> {
+) -> Result<Vec<usize>> {
     let settings = cifly::Settings::new(verbose, true);
 
     let parsed_ruletable;
@@ -54,11 +54,11 @@ fn reach(
         Ok(rt) => &rt.0,
         Err(_) => {
             parsed_ruletable = to_ruletable(
-                ruletable
-                    .as_str()
-                    .expect("Error: expected a string as ruletable argument."),
+                ruletable.as_str().ok_or(extendr_api::Error::from(
+                    "Error: expected a string as ruletable argument.",
+                ))?,
                 tableAsString,
-            );
+            )?;
             &parsed_ruletable
         }
     };
@@ -67,7 +67,7 @@ fn reach(
     let graph_ref = match <&Graph>::try_from(&graph) {
         Ok(g) => &g.0,
         Err(_) => {
-            parsed_graph = to_graph(&graph, ruletable_ref);
+            parsed_graph = to_graph(&graph, ruletable_ref)?;
             &parsed_graph
         }
     };
@@ -76,15 +76,17 @@ fn reach(
     let sets_ref = match <&Sets>::try_from(&sets) {
         Ok(z) => &z.0,
         Err(_) => {
-            parsed_sets = to_sets(&sets, ruletable_ref);
+            parsed_sets = to_sets(&sets, ruletable_ref)?;
             &parsed_sets
         }
     };
 
-    cifly::reach::reach(graph_ref, sets_ref, ruletable_ref, &settings)
-        .iter()
-        .map(|&x| x + 1)
-        .collect()
+    Ok(
+        cifly::reach::reach(graph_ref, sets_ref, ruletable_ref, &settings)
+            .iter()
+            .map(|&x| x + 1)
+            .collect(),
+    )
 }
 
 #[extendr]
@@ -116,13 +118,13 @@ impl Ruletable {}
 /// reach(edgelist, sets, rt)
 /// @export
 #[extendr]
-fn parseRuletable(ruletable: Robj, #[default = "FALSE"] tableAsString: bool) -> Ruletable {
-    Ruletable(to_ruletable(
-        ruletable
-            .as_str()
-            .expect("Error: expected a string as ruletable argument."),
+fn parseRuletable(ruletable: Robj, #[default = "FALSE"] tableAsString: bool) -> Result<Ruletable> {
+    Ok(Ruletable(to_ruletable(
+        ruletable.as_str().ok_or(extendr_api::Error::from(
+            "Error: expected a string as ruletable argument.",
+        ))?,
         tableAsString,
-    ))
+    )?))
 }
 
 #[extendr]
@@ -155,21 +157,25 @@ impl Graph {}
 /// reach(edgelist, sets, dsepTable, tableAsString=TRUE)
 /// @export
 #[extendr]
-fn parseGraph(graph: Robj, ruletable: Robj, #[default = "FALSE"] tableAsString: bool) -> Graph {
+fn parseGraph(
+    graph: Robj,
+    ruletable: Robj,
+    #[default = "FALSE"] tableAsString: bool,
+) -> Result<Graph> {
     let parsed_ruletable;
     let ruletable_ref = match <&Ruletable>::try_from(&ruletable) {
         Ok(rt) => &rt.0,
         Err(_) => {
             parsed_ruletable = to_ruletable(
-                ruletable
-                    .as_str()
-                    .expect("Error: expected a string as ruletable argument."),
+                ruletable.as_str().ok_or(extendr_api::Error::from(
+                    "Error: expected a string as ruletable argument.",
+                ))?,
                 tableAsString,
-            );
+            )?;
             &parsed_ruletable
         }
     };
-    Graph(to_graph(&graph, ruletable_ref))
+    Ok(Graph(to_graph(&graph, ruletable_ref)?))
 }
 
 #[extendr]
@@ -202,96 +208,120 @@ impl Sets {}
 /// reach(edgelist, s, dsepTable, tableAsString=TRUE)
 /// @export
 #[extendr]
-fn parseSets(sets: Robj, ruletable: Robj, #[default = "FALSE"] tableAsString: bool) -> Sets {
+fn parseSets(
+    sets: Robj,
+    ruletable: Robj,
+    #[default = "FALSE"] tableAsString: bool,
+) -> Result<Sets> {
     let parsed_ruletable;
     let ruletable_ref = match <&Ruletable>::try_from(&ruletable) {
         Ok(rt) => &rt.0,
         Err(_) => {
             parsed_ruletable = to_ruletable(
-                ruletable
-                    .as_str()
-                    .expect("Error: expected a string as ruletable argument."),
+                ruletable.as_str().ok_or(extendr_api::Error::from(
+                    "Error: expected a string as ruletable argument.",
+                ))?,
                 tableAsString,
-            );
+            )?;
             &parsed_ruletable
         }
     };
-    Sets(to_sets(&sets, ruletable_ref))
+    Ok(Sets(to_sets(&sets, ruletable_ref)?))
 }
 
-fn to_ruletable(ruletable_str: &str, as_string: bool) -> cifly::Ruletable {
+fn to_ruletable(ruletable_str: &str, as_string: bool) -> Result<cifly::Ruletable> {
     let ruletable_res = if as_string {
         cifly::Ruletable::from_multiline_string(ruletable_str)
     } else {
         cifly::Ruletable::from_file(ruletable_str)
     };
 
-    ruletable_res
-        .map_err(|err| format!("Error: could not read ruletable {}: {}", ruletable_str, err))
-        .unwrap()
+    ruletable_res.map_err(|err| {
+        extendr_api::Error::from(format!(
+            "Error: could not read ruletable {}: {}",
+            ruletable_str, err
+        ))
+    })
 }
 
-fn to_graph(graph: &Robj, ruletable: &cifly::Ruletable) -> cifly::Graph {
-    if !graph.is_list() {
-        panic!("edges should be given as list");
-    }
-    let vecs = graph.as_list().unwrap().into_hashmap();
+fn to_graph(graph: &Robj, ruletable: &cifly::Ruletable) -> Result<cifly::Graph> {
+    let vecs = graph
+        .as_list()
+        .ok_or(extendr_api::Error::from(
+            "Error: edges should be given as list",
+        ))?
+        .into_hashmap();
     let mut edge_lists = HashMap::new();
     for (edge_string, v) in vecs.iter() {
         if !v.is_null() && !v.is_matrix() {
-            panic!("Error: each edge list should be given as x times 2 matrix of integers");
+            return Err(extendr_api::Error::from(
+                "Error: each edge list should be given as x times 2 matrix of integers",
+            ));
         }
         let edge_list = if !v.is_null() {
             if v.is_integer() {
-                let edge_list_r: RMatrix<i32> = v
-                    .as_matrix()
-                    .expect("Error: edge list should be given as x times 2 matrix");
+                let edge_list_r: RMatrix<i32> = v.as_matrix().ok_or(extendr_api::Error::from(
+                    "Error: edge list should be given as x times 2 matrix expected a string as ruletable argument.",
+                ))?;
                 let num_edges = edge_list_r.nrows();
                 let edge_list_raw = edge_list_r.data();
-                (0..num_edges)
-                    .map(|i| {
-                        (
-                            i32_to_node_id(edge_list_raw[i]),
-                            i32_to_node_id(edge_list_raw[i + num_edges]),
-                        )
+                let (first_half, second_half) = edge_list_raw.split_at(num_edges);
+
+                first_half
+                    .iter()
+                    .zip(second_half.iter())
+                    .map(|(&a_raw, &b_raw)| {
+                        let a = i32_to_node_id(a_raw)?;
+                        let b = i32_to_node_id(b_raw)?;
+                        Ok((a, b))
                     })
-                    .collect()
+                    .collect::<Result<Vec<_>>>()?
             } else if v.is_real() {
-                let edge_list_r: RMatrix<f64> = v
-                    .as_matrix()
-                    .expect("Error: edge list should be given as x times 2 matrix");
+                let edge_list_r: RMatrix<f64> = v.as_matrix().ok_or(extendr_api::Error::from(
+                    "Error: edge list should be given as x times 2 matrix",
+                ))?;
+
                 let num_edges = edge_list_r.nrows();
                 let edge_list_raw = edge_list_r.data();
-                (0..num_edges)
-                    .map(|i| {
-                        (
-                            f64_to_node_id(edge_list_raw[i]),
-                            f64_to_node_id(edge_list_raw[i + num_edges]),
-                        )
+
+                let (first_half, second_half) = edge_list_raw.split_at(num_edges);
+
+                first_half
+                    .iter()
+                    .zip(second_half.iter())
+                    .map(|(&a_raw, &b_raw)| {
+                        let a = f64_to_node_id(a_raw)?;
+                        let b = f64_to_node_id(b_raw)?;
+                        Ok((a, b))
                     })
-                    .collect()
+                    .collect::<Result<Vec<_>>>()?
             } else {
-                panic!(
-                    "Error: edge list matrix contains neither integers nor floating point numbers"
-                )
+                return Err(extendr_api::Error::from(
+                    "Error: edge list matrix contains neither integers nor floating point numbers",
+                ));
             }
         } else {
             Vec::new()
         };
         edge_lists.insert(edge_string.to_string(), edge_list);
     }
-    cifly::Graph::new(&edge_lists, ruletable).unwrap()
+    cifly::Graph::new(&edge_lists, ruletable)
+        .map_err(|err| extendr_api::Error::from(format!("Error: could not read graph: {}", err)))
 }
 
-fn to_sets(sets: &Robj, ruletable: &cifly::Ruletable) -> cifly::Sets {
-    if !sets.is_list() {
-        panic!("Error: sets should be given as list");
-    }
-    let vecs = sets.as_list().unwrap().into_hashmap();
+fn to_sets(sets: &Robj, ruletable: &cifly::Ruletable) -> Result<cifly::Sets> {
+    let vecs = sets
+        .as_list()
+        .ok_or(extendr_api::Error::from(
+            "Error: sets should be given as list",
+        ))?
+        .into_hashmap();
     let mut set_lists = HashMap::new();
     for (set_string, v) in vecs.iter() {
         if !v.is_vector() && !v.is_number() && !v.is_null() {
-            panic!("Error: each set should be a vector");
+            return Err(extendr_api::Error::from(
+                "Error: each set should be a vector",
+            ));
         }
         let s;
         if v.is_null() {
@@ -299,38 +329,47 @@ fn to_sets(sets: &Robj, ruletable: &cifly::Ruletable) -> cifly::Sets {
         } else if v.is_integer() {
             s = v
                 .as_integer_slice()
-                .unwrap()
+                .ok_or(extendr_api::Error::from(
+                    "Error: each should be given as vector",
+                ))?
                 .iter()
                 .map(|&x| i32_to_node_id(x))
-                .collect();
+                .collect::<Result<Vec<_>>>()?;
         } else if v.is_real() {
             s = v
                 .as_real_slice()
-                .unwrap()
+                .ok_or(extendr_api::Error::from(
+                    "Error: each should be given as vector",
+                ))?
                 .iter()
                 .map(|&x| f64_to_node_id(x))
-                .collect();
+                .collect::<Result<Vec<_>>>()?;
         } else {
             panic!("Error: set vector contains neither integers nor floating point numbers");
         }
         set_lists.insert(set_string.to_string(), s);
     }
-    cifly::Sets::new(&set_lists, ruletable).unwrap()
+    cifly::Sets::new(&set_lists, ruletable)
+        .map_err(|err| extendr_api::Error::from(format!("Error: could not read sets: {}", err)))
 }
 
-fn i32_to_node_id(id: i32) -> usize {
+fn i32_to_node_id(id: i32) -> Result<usize> {
     if id >= 1 {
-        (id as usize) - 1
+        Ok((id as usize) - 1)
     } else {
-        panic!("Error: expected positive integer as node id")
+        Err(extendr_api::Error::from(
+            "Error: expected positive integer as node id",
+        ))
     }
 }
 
-fn f64_to_node_id(id: f64) -> usize {
+fn f64_to_node_id(id: f64) -> Result<usize> {
     let rounded_id = id.round();
     if rounded_id >= 1.0 {
-        (rounded_id as usize) - 1
+        Ok((rounded_id as usize) - 1)
     } else {
-        panic!("Error: expected positive integer as node id")
+        Err(extendr_api::Error::from(
+            "Error: expected positive integer as node id",
+        ))
     }
 }
