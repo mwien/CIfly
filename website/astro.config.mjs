d88ec1dc6005b259612b270a0814ec5dc5da59f4
Marcel Wienöbst <marcel.wienoebst@gmx.de>
2025-06-19 20:58:02 +0200

    [website] add sitemap integration

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -8,6 +8,8 @@ import CiflyGrammar from "./src/shiki/grammar/cifly.tmLanguage.json";
 import customCatppucinLatte from "./src/shiki/theme/catppuccin-latte.json";
 import customCatppucinMocha from "./src/shiki/theme/catppuccin-mocha.json";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
@@ -19,9 +21,11 @@ export default defineConfig({
       themes: { light: customCatppucinLatte, dark: customCatppucinMocha },
     },
   },
-  integrations: [mdx()],
+  site: "https://cifly.pages.dev",
+  integrations: [mdx(), sitemap()],
   trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
