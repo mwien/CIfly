66b5b5641a942d54cdbef7632a82975691d4845b
Leonard Henckel <69041654+henckell@users.noreply.github.com>
2025-06-25 11:43:53 +0100

    optimal VAS addition

---
+++ examples/ciflyr-examples/R/optimalAdjustment.R
@@ -0,0 +1,29 @@
+library("ciflyr") 
+library("here")
+
+ruletables <- getRuletablePath()
+notAmenable <- parseRuletable(file.path(ruletables, "not_amenable_cpdag.txt"))
+possibleAncestors <- parseRuletable(file.path(ruletables, "possible_ancestors_cpdag.txt"))
+possibleDescendants <- parseRuletable(file.path(ruletables, "possible_descendants_cpdag.txt"))
+parents <- here(ruletables, "parents_cpdag.txt")
+
+optimalAdj <- function(cpdag,X,Y){
+  des <- reach(cpdag, list("X" = X), possibleDescendants)
+  if(length(intersect(Y,des)) < length(Y)){
+    print("\nNot all nodes in Y are possible descendants of X \n")
+    return(FALSE)
+  }
+  
+  nam <- reach(cpdag, list("X" = X), notAmenable)
+  anc <- reach(cpdag, list("X" = Y, "W" = X), possibleAncestors)
+  cn <- intersect(anc, des)
+  forb <- reach(cpdag, list("X" = cn), possibleDescendants)
+  if (length(intersect(forb, X)) != 0 | length(intersect(nam, Y)) != 0) {
+    print("There is no valid adjustment set relative to (X,Y) in the given graph.\n")
+    return(FALSE)
+  }
+  
+  pre_opt <- reach(cpdag,list("X"=cn),parents)
+  
+  return(setdiff(pre_opt,union(forb,X)))
+}
